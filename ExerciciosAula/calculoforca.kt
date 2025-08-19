97a88eab3adf102544c88eb0a1e8e13107886d1b
foxczie <shiguemoriale@gmail.com>
2025-08-18 22:32:52 -0300

    Corrigindo identações

--- ExerciciosAula/calculoforca.kt
+++ ExerciciosAula/calculoforca.kt
@@ -1,11 +1,11 @@
 
 fun main() {
     println("Qual o valor da massa?")
-    val m = readln().toDouble()
+    val m = readln().toDouble() // variável que lê a massa
     println("Qual o valor da aceleração?")
-    val a = readln().toDouble()
-    println("O valor da força é: ${calculoForca(m,a)}")
+    val a = readln().toDouble() // variável que lê a aceleração
+    println("O valor da força é: ${calculoForca(m,a)}") // chamada de função
 }
 fun calculoForca(m: Double, a: Double): Double {
-    return m * a
+    return m * a // retorno do cálculo da força
 }
