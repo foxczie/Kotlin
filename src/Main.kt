3cb540adbe10a60eccc7a63dc8115a659a290915
foxczie <shiguemoriale@gmail.com>
2025-08-18 22:21:36 -0300

    Exercício de cálculo de força

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Qual o valor da massa?")
+    val m = readln().toDouble()
+    println("Qual o valor da aceleração?")
+    val a = readln().toDouble()
+    println("O valor da força é: ${calculoForca(m,a)}")
+}
+fun calculoForca(m: Double, a: Double): Double {
+    return m * a
+}
